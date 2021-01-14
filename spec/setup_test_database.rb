1b7790659f5381a5a93d066596422828e4b01ec2
Tuyet Le <toowee.le@gmail.com>
2021-01-14 16:02:27 +0000

    Add helper method to reset bookmarks table in the test database before each test run

---
+++ spec/setup_test_database.rb
@@ -0,0 +1,9 @@
+require 'pg'
+
+p "Setting up test database..."
+
+# Connect to the test database
+connection = PG.connect( dbname: 'bookmark_manager_test' )
+
+# Reset the bookmarks table
+connection.exec( "TRUNCATE bookmarks" )
