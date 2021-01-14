c6f45192eda9486aaa8fb374eb42d806be14919b
Tuyet Le <toowee.le@gmail.com>
2021-01-14 15:35:14 +0000

    Fix unit test by adding test values

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,12 @@ require 'Bookmark'
 describe Bookmark do
   describe ".all" do
     it "returns bookmark list" do
+      # Connect to the testing database
+      connection = PG.connect( dbname: 'bookmark_manager_test' )
+      # Add test data
+      connection.exec( "INSERT INTO bookmarks (url) VALUES('http://www.google.com');" )
+      connection.exec( "INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');" )
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.google.com")
