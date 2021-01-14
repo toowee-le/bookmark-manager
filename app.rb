e7f16da8a52de7090ea140f4cab3555860dcdeb5
Tuyet Le <toowee.le@gmail.com>
2021-01-14 23:18:30 +0000

    Add a form to post new data to database and /bookmarks

--- app.rb
+++ app.rb
@@ -11,5 +11,17 @@ class BookmarkManager < Sinatra::Base
     erb :'bookmarks'
   end
 
+  get "/bookmarks/new" do
+    erb :'bookmarks/new'
+  end
+
+  post "/bookmarks" do
+    name = params['site_name']
+    url = params['site_url']
+    connection = PG.connect( dbname: "bookmark_manager" )
+    connection.exec( "INSERT INTO bookmarks (name, url) VALUES('#{name}', '#{url}');" )
+    redirect :'bookmarks'
+  end
+
   run! if app_file == $0
 end
