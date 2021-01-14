44eefbac133230c914a6aac9a5a86b261a80a2be
Tuyet Le <toowee.le@gmail.com>
2021-01-14 15:32:47 +0000

    Refactor model to use ENVto determine which database to connect

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,7 +2,12 @@ require 'pg'
 
 class Bookmark
   def self.all
-    connection = PG.connect( dbname: 'bookmark_manager' )
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect( dbname: 'bookmark_manager_test' )
+    else
+      connection = PG.connect( dbname: 'bookmark_manager' )
+    end
+    
     data = connection.exec( 'SELECT * FROM bookmarks' )
     data.collect { |bookmark| bookmark['url']}
   end
