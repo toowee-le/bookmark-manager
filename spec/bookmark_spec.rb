20953b4c0863241250710286cfc95faf8ca682b3
Tuyet Le <toowee.le@gmail.com>
2021-01-15 21:08:51 +0000

    Update comments

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,8 +3,9 @@ require 'Bookmark'
 describe Bookmark do
   describe ".all" do
     it "returns bookmark list" do
-      # Connect to the testing database
+      # Connect to the test database
       connection = PG.connect( dbname: 'bookmark_manager_test' )
+
       # Add test data
       connection.exec( "INSERT INTO bookmarks (name, url) VALUES('Google', 'http://www.google.com');" )
       connection.exec( "INSERT INTO bookmarks (name, url) VALUES('Makers Academy', 'http://www.makersacademy.com');" )
@@ -22,8 +23,6 @@ describe Bookmark do
 
       bookmarks = Bookmark.all
 
-      # expect(bookmark.name).to eq "Google"
-      # expect(bookmark.url).to eq "http://www.google.com"
       expect(bookmarks).to include(name: "Google", url:"http://www.google.com")
     end
   end
