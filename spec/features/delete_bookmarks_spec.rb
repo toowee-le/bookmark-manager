de625f091929593c252e57ae85bb46a4b0c17791
Tuyet Le <toowee.le@gmail.com>
2021-01-30 14:47:07 +0000

    Refactor crud delete test

--- spec/features/delete_bookmarks_spec.rb
+++ spec/features/delete_bookmarks_spec.rb
@@ -1,12 +1,22 @@
-feature "Delete bookmarks" do
-  scenario "A user can delete a bookmark" do
-    Bookmark.create(name: "Google", url: "http://www.google.com")
-    visit("/bookmarks")
-    expect(page).to have_link("Google", href: "http://www.google.com")
-    
-    first('.bookmark').click_button "Delete"
+feature 'Deleting bookmarks' do
+  before { Bookmark.create(name: 'Google', url: 'http://www.google.com') }
+  before { Bookmark.create(name: 'Makers', url: 'https://www.makers.tech') }
 
-    expect(current_path).to eq "/bookmarks"
-    expect(page).not_to have_link("Google", href: "http://www.google.com")
+  scenario 'user can delete a bookmark' do
+    bookmarks = Bookmark.all
+    first_bookmark, second_bookmark = bookmarks[0], bookmarks[1]
+
+    visit('/bookmarks')
+
+    expect(page).to have_link(first_bookmark.name, href: first_bookmark.url)
+    expect(page).to have_link(second_bookmark.name, href: second_bookmark.url)
+    
+    within "#bookmark-#{first_bookmark.id}" do
+      click_button 'Delete'
+    end
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link(first_bookmark.name, href: first_bookmark.url)
+    expect(page).to have_link(second_bookmark.name, href: second_bookmark.url)
   end
 end
